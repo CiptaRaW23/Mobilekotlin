50b42c10d27e02a4e07cc5de5ede8ea6713ccdd5
CRANGGAW\User <crwijaya15@email.com>
2024-02-26 15:39:15 +0700

    Cipta

---
+++ app/src/main/kotlin/main.kt
@@ -0,0 +1,18 @@
+
+
+fun main(args: Array<String>) {
+    biodata("Cipta Rangga Wijaya", 18)
+    hobby("Mancing", "Saya lakukan pada saat liburan kuliah, hal yang paling seru saat memancing yaitu menunggu sambaran ikan")
+}
+
+fun biodata(name:String, age:Int){ // minimal 2 parameter/args
+    println("=====================")
+    println("Nama\t: $name")
+    println("Usia\t: $age")
+    println("=====================")
+}
+
+fun hobby(nama:String, desc:String){
+    println("Hobby saya $nama")
+    println("Hobby tersebut $desc")
+}
